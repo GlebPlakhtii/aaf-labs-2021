b4303ee8067faec9746cdecaf23869587894c93b
Gleb Plakhtii <50776222+GlebPlakhtii@users.noreply.github.com>
2021-10-28 22:39:02 +0300

    add compiler.cpp

---
+++ plakhtii_fi-92_shevtsova_fi-92/R_Tree_To_compile/compiler.cpp
@@ -0,0 +1,20 @@
+#include "R_Tree.h"
+#include "Rect.h"
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(R_Tree, m) {
+py::class_<Rect>(m, "Rect")
+.def(py::init<int,int,int,int>(), py::arg("ltx"),py::arg("lty"),py::arg("rbx"),py::arg("rby"))
+.def("__repr__", &Rect::to_string);
+
+py::class_<R_Tree>(m, "R_Tree")
+.def(py::init<>())
+.def("insert", &R_Tree::incert, "insert Rect into tree", py::arg("Rect"))
+.def("search", &R_Tree::search , "search elements in rect", py::arg("Rect"))
+.def("to_string", &R_Tree::to_string, "tree to string")
+.def("print_tree", &R_Tree::print_tree, "print tree");
+
+
+}
